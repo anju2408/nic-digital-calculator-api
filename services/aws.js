0ada458f0f8c9e3437fac6b7f0195f2ad74aedd3
divyam-datafortune <66360045+divyam-datafortune@users.noreply.github.com>
2020-07-16 00:39:20 +0530

    Feature/generate pdf (#4)
    Co-authored-by: Divya Mahajan <divya.mahajan@neosofttech.com>
    Co-authored-by: Alexey Kulakov <uni_que@me.com>

--- services/aws.js
+++ services/aws.js
@@ -3,10 +3,9 @@ const aws = require('aws-sdk');
 /**
  *To save pdf files on s3 bucket
  * @param pdf - request data
- * @returns {Promise<void>}
+ * @returns Pdf file  public url
  */
-module.exports.uploadPdfBuffer = async (buffer) => {
-
+module.exports.uploadPdfBuffer = async (pdfBuffer) => {
   const s3 = new aws.S3({
     accessKeyId: process.env.CLOUDCUBE_ACCESS_KEY_ID,
     secretAccessKey: process.env.CLOUDCUBE_SECRET_ACCESS_KEY,
@@ -14,12 +13,13 @@ module.exports.uploadPdfBuffer = async (buffer) => {
   
   const fileName = `vztyis1bvhuk/public/NICE-InContact-Recommendation-Report-${Date.now()}.pdf`;
   const filePath = `${process.env.CLOUDCUBE_URL}/${fileName}`;
-  const uploadParams = { Bucket: 'cloud-cube', Key: fileName, Body: buffer };
 
-  await s3.upload(uploadParams, function (err, data) {
+  const uploadParams = { Bucket: 'cloud-cube', Key: fileName, Body: pdfBuffer };
+
+  s3.upload(uploadParams, (err) => {
     if (err) {
-      console.log("Error", err);
-      return false
+      // eslint-disable-next-line no-console
+      console.log('Error', err);
     }
   });
   return filePath;
